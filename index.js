ba4a2d46c87dca11bd2c50a9ddf13ac5390f015d
TiagoDanin <TiagoDanin@outlook.com>
2018-10-19 13:35:26 -0300

    Fix parse string

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const next = (index, tags) => {
 	return tags.splice(index+1, tags.length).join(' ')
 }
 
-const isString = (str) => {
-	if (typeof str == 'string' && str.startsWith('"')) {
-		return (str.replace(/^"/, '')).replace(/"$/, '')
-	}
-	return false
-}
-
 const parse = (input, html) => {
 	if (Array.isArray(input)) {
 		input = input.join(' ')
@@ -25,17 +18,28 @@ const parse = (input, html) => {
 		*/
 		if (!total && total === false) {
 			return false
-		} else if (total && total.output) {
-			return false
 		} else if (total && total.stop) {
 			return total
+		} else if (total && total.isString) {
+			if (current === '"') {
+				total.stop = true
+				total.output += next(index, tags)
+				return total
+			}
+			total.output += current
+			return total
 		}
+
 		switch(current) {
 			case '!!': {
 				var comment = next(index, tags)
 				if (input.startsWith('!!'))	{
 					total.output = comment
 				}
+				break
+			}
+			case '"': {
+				total.isString = true
 				break
 			}
 			case '>>': {
@@ -45,9 +49,7 @@ const parse = (input, html) => {
 			case '==': {
 				total.output = parse(total.select, html)
 				var nextIndex = next(index, tags)
-				if (isString(nextIndex) && isString(nextIndex) === total.output) {
-					return total
-				} else if (total.output === parse(nextIndex, html)) {
+				if (total.output === parse(nextIndex, html)) {
 					break
 				}
 				return false
@@ -62,22 +64,14 @@ const parse = (input, html) => {
 				}
 
 				var nextIndex = next(index, tags)
-				if (isString(nextIndex)) {
-					total.output += isString(nextIndex)
-					break
-				}
 				total.output = parse(nextIndex, html)
 				break
 			}
 			case '++': {
 				total.output = parse(total.select, html)
 				var nextIndex = next(index, tags)
-				if (isString(nextIndex)) {
-					total.output += isString(nextIndex)
-					break
-				}
-
 				var addOutput = parse(nextIndex, html)
+
 				if (Array.isArray(total.output)) {
 					total.select = [].concat(total.output, addOutput)
 				} else if (typeof total.output == 'object') {
@@ -91,19 +85,14 @@ const parse = (input, html) => {
 			}
 			case '--': {
 				var nextIndex = next(index, tags)
-				var removeRaw = ''
+				var removeRaw = parse(nextIndex, html)
 				var output = parse(total.select, html)
-				if (isString(nextIndex)) {
-					removeRaw = isString(nextIndex)
-				} else {
-					removeRaw = parse(nextIndex, html)
-				}
 
 				if (output && output.replace && typeof output.replace == 'function') {
 					total.output = output.replace(removeRaw, '')
-				} else {
-					total.output = output - removeRaw
+					break
 				}
+				total.output = output - removeRaw
 				break
 			}
 			case '@>': {
@@ -120,7 +109,8 @@ const parse = (input, html) => {
 		select: '',
 		output: '',
 		table: false,
-		stop: false
+		stop: false,
+		isString: false
 	})
 
 	if (!data) {
@@ -234,7 +224,7 @@ console.log(parse('div >> h2 @> label', html))
 //Hello world, Hello world, Hello world
 */
 
-/*
+
 var html = cheerio.load(`
 <div>
 	<h1>Tiago</h1>
@@ -243,14 +233,14 @@ var html = cheerio.load(`
 `)
 console.log(parse('h1 ++ h2', html))
 //TiagoDanin
-console.log(parse('h1 ++ "2018"', html))
+console.log(parse('h1 ++ #> 2018 ##', html))
 //Tiago2018
 console.log(parse('h3 || h1', html))
 //Tiago
-console.log(parse('h3 || "2018"', html))
+console.log(parse('h3 || #> 2018 ##', html))
 //2018
-console.log(parse('h3 || h4 || h1 ++ h2 ++ "."', html))
+console.log(parse('h3 || h4 || h1 ++ h2 ++ #> . ##', html))
 //TiagoDanin.
-console.log(parse('h1 -- "Tia"', html))
+console.log(parse('h1 -- " Tia "', html))
 //go
-*/
+" "
