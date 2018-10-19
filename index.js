138cbc550a24120d86be74f618a13e54d33bb7f6
TiagoDanin <TiagoDanin@outlook.com>
2018-10-19 12:16:26 -0300

    Fix get children elem

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function parse(input, html) {
+const next = (index, tags) => {
+	return tags.splice(index+1, tags.length).join(' ')
+}
+
+const isString = (str) => {
+	if (typeof str == 'string' && str.startsWith('"')) {
+		return (str.replace(/^"/, '')).replace(/"$/, '')
+	}
+	return false
+}
+
+const parse = (input, html) => {
 	if (Array.isArray(input)) {
 		input = input.join(' ')
 	}
+
 	var tags = input.toString().split(' ')
 	var data = tags.reduce((total, current, index) => {
 		/*
@@ -20,7 +32,7 @@ function parse(input, html) {
 		}
 		switch(current) {
 			case '!!': {
-				var comment = tags.splice(index+1, tags.length).join(' ')
+				var comment = next(index, tags)
 				if (input.startsWith('!!'))	{
 					total.output = comment
 				}
@@ -32,12 +44,9 @@ function parse(input, html) {
 			}
 			case '==': {
 				total.output = parse(total.select, html)
-				var nextIndex = tags.splice(index+1, tags.length).join(' ')
-				if (nextIndex.startsWith('"')) {
-					if (total.output === (nextIndex.replace(/^"/, '')).replace(/"$/, '')) {
-						break
-					}
-					return false
+				var nextIndex = next(index, tags)
+				if (isString(nextIndex) && isString(nextIndex) === total.output) {
+					return total
 				} else if (total.output === parse(nextIndex, html)) {
 					break
 				}
@@ -51,32 +60,54 @@ function parse(input, html) {
 					total.stop = true
 					break
 				}
-				var nextIndex = tags.splice(index+1, tags.length).join(' ')
+
+				var nextIndex = next(index, tags)
+				if (isString(nextIndex)) {
+					total.output += isString(nextIndex)
+					break
+				}
 				total.output = parse(nextIndex, html)
 				break
 			}
 			case '++': {
 				total.output = parse(total.select, html)
-				var nextIndex = tags.splice(index+1, tags.length).join(' ')
-				var addOutput = parse(nextIndex html)
+				var nextIndex = next(index, tags)
+				if (isString(nextIndex)) {
+					total.output += isString(nextIndex)
+					break
+				}
+
+				var addOutput = parse(nextIndex, html)
 				if (Array.isArray(total.output)) {
 					total.select = [].concat(total.output, addOutput)
-				} else if (OBJ) {
+				} else if (typeof total.output == 'object') {
 					total.output = {
-						...parse(nextIndex, addOutput),
+						...addOutput,
 						...total.output
 					}
 				}
-				total.output += parse(nextIndex, addOutput)
+				total.output += addOutput
 				break
 			}
 			case '--': {
-				var nextIndex = tags.splice(index+1, tags.length).join(' ')
-				total.output = parse(total.select, html) - parse(nextIndex, html)
+				var nextIndex = next(index, tags)
+				var removeRaw = ''
+				var output = parse(total.select, html)
+				if (isString(nextIndex)) {
+					removeRaw = isString(nextIndex)
+				} else {
+					removeRaw = parse(nextIndex, html)
+				}
+
+				if (output && output.replace && typeof output.replace == 'function') {
+					total.output = output.replace(removeRaw, '')
+				} else {
+					total.output = output - removeRaw
+				}
 				break
 			}
 			case '@>': {
-				total.table = tags.splice(index+1, tags.length).join(' ')
+				total.table = next(index, tags)
 				break
 			}
 			default: {
@@ -115,7 +146,7 @@ function parse(input, html) {
 			select = (select.replace(/^\[/, '')).replace(/]$/, '')
 		}
 
-		var table = html(data.select).contents().map((index, elem) => {
+		var table = html(data.select).map((index, elem) => {
 			var parseRaw = parse(select, html(elem))
 			if (type == 'object') {
 				var arrayOutput = {}
@@ -133,12 +164,13 @@ function parse(input, html) {
 		if (data.select === '') {
 			return html.text()
 		}
+
 		if (typeof html == 'function') {
 			return html(data.select).text()
-		} else if (typeof html.text == 'function') {
-				return html.text()
 		} else if (typeof html == 'object' && typeof html.children == 'function') {
-			return html.children(data.select)
+			return html.children(data.select).text()
+		} else if (typeof html.text == 'function') {
+			return html.text()
 		}
 		//TODO: Error
 		return 'ERRRO!'
@@ -157,6 +189,8 @@ var html = cheerio.load(`
 console.log(parse('!! my comment in ScrapperScript', html))
 //my comment in ScrapperScript
 console.log(parse('div >> h2 !! my comment in ScrapperScript', html))
+//Hello world
+console.log(parse('div >> h2 == "Hello world"', html))
 //Hello world
 */
 
@@ -180,13 +214,12 @@ console.log(parse('div >> h2 @> ', html))
 //Hello world, Hello world, Hello world
 */
 
-
 /*
 var html = cheerio.load(`
 <div class="title">
-	<h2><label>Hello world</label></h2>
-	<h2><label>Hello world</label></h2>
-	<h2><label>Hello world</label></h2>
+	<h2>Title: <label>Hello world</label></h2>
+	<h2>Title: <label>Hello world</label></h2>
+	<h2>Title: <label>Hello world</label></h2>
 </div>
 `)
 console.log(parse('div >> h2 @> {number, text, label == "Hello world"}', html))
@@ -199,4 +232,25 @@ console.log(parse('div >> h2 @> [label]', html))
 //[ 'Hello world', 'Hello world', 'Hello world' ]
 console.log(parse('div >> h2 @> label', html))
 //Hello world, Hello world, Hello world
+*/
+
+/*
+var html = cheerio.load(`
+<div>
+	<h1>Tiago</h1>
+	<h2>Danin</h2>
+</div>
+`)
+console.log(parse('h1 ++ h2', html))
+//TiagoDanin
+console.log(parse('h1 ++ "2018"', html))
+//Tiago2018
+console.log(parse('h3 || h1', html))
+//Tiago
+console.log(parse('h3 || "2018"', html))
+//2018
+console.log(parse('h3 || h4 || h1 ++ h2 ++ "."', html))
+//TiagoDanin.
+console.log(parse('h1 -- "Tia"', html))
+//go
 */
