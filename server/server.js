26bb83d1fa00cf3cbc3d284b3de0105315804745
TiagoDanin <TiagoDanin@outlook.com>
2019-06-09 15:12:48 -0300

    fix: set default port

--- server/server.js
+++ server/server.js
@@ -1,27 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app_express = express()
-const { parseFile } = require('../src')
 
-const port = process.env.PORT
-if (!port) {
-	console.log('Set env: PORT')
-	console.log('E.g: export PORT=3000')
-	return
-}
+const app = express()
+const {parseFile} = require('../src')
 
-app_express.set('port', process.env.PORT)
-app_express.use(bodyParser.text())
+app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.text())
 
-app_express.get('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.send('ScrapperScript Server by Tiago Danin')
 })
 
-app_express.post('/', async (req, res) => {
-	var data = await parseFile(req.body)
+app.post('/', async (req, res) => {
+	const data = await parseFile(req.body)
 	res.json(data)
 })
 
-app_express.listen(app_express.get('port'), () => {
-	console.log('[+] Port: ', app_express.get('port'))
+app.listen(app.get('port'), () => {
+	console.log('[+] Port:', app.get('port'))
 })
